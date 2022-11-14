57c5e652ffcc59f62a737eb1bc94facf51045224
TRAVIS HEAVENER (1035058) <1035058@lcps.org>
2022-11-14 17:50:19 -0500

    Incomplete commit - transferring to PC

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,35 @@
-import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { Pedometer } from 'expo-sensors';
 
 const HomeScreen = (props) => {
+    const epoch = new Date();
+    const [ped, setPed] = useState(0);
+    
+    // check if pedometer features are enabled
+    (async () => console.log(await Pedometer.isAvailableAsync()))();
+
+    const updateSteps = result => {
+        setPed(result.steps);
+        console.log("updated");
+    };
+
+    useEffect( () => {
+        Pedometer.watchStepCount(updateSteps);
+        console.log("effected");
+    }, [updateSteps]);
+
+    const getSteps = () => {
+        console.log(ped);
+        updateSteps({steps: 0});
+    };
+    
     return (
         <View>
-            <Text>asdf</Text>
+            <Text>Steps: {ped}</Text>
+            <TouchableOpacity onPress={getSteps}>
+                <Text>Update steps</Text>
+            </TouchableOpacity>
         </View>
     );
 };
