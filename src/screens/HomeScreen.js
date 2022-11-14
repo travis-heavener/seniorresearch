7292c4fd2d944a7ef97cb0054f6e73f5d1661c28
TRAVIS HEAVENER (1035058) <1035058@lcps.org>
2022-11-14 12:34:39 -0500

    Switch App.js to use RN stack navigation

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const HomeScreen = (props) => {
+    return (
+        <View>
+            <Text>asdf</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default HomeScreen;
