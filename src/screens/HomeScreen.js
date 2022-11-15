0f25953547d07904d7bd1041a7c14b2ab4ad53ab
Travis H <travis.heavener@gmail.com>
2022-11-15 00:24:32 -0500

    WIP: Perms for ACTIVITY_RECOGNITION

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,22 +2,40 @@ import React, { useState, useEffect } from "react";
 import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
 import { Pedometer } from 'expo-sensors';
 
+import { PermissionsAndroid } from 'react-native';
+
 const HomeScreen = (props) => {
     const epoch = new Date();
     const [ped, setPed] = useState(0);
     
     // check if pedometer features are enabled
-    (async () => console.log(await Pedometer.isAvailableAsync()))();
+    (async () => console.log("Pedometer Enabled: " + await Pedometer.isAvailableAsync()))();
 
-    const updateSteps = result => {
+	let request = async () => {
+		try {
+			const res = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACTIVITY_RECOGNITION, {
+				title: "Cool Photo App Camera ACTIVITY_RECOGNITION",
+				message: "Cool Photo App needs access to your camera so you can take awesome pictures.",
+				buttonNeutral: "Ask Me Later",
+				buttonNegative: "Cancel",
+				buttonPositive: "OK"
+			});
+			console.log("Result? " + res);
+		} catch (e) {
+			console.warn(e);
+		}
+      };
+
+	const updateSteps = result => {
         setPed(result.steps);
-        console.log("updated");
+        console.log("updated steps function");
     };
 
     useEffect( () => {
         Pedometer.watchStepCount(updateSteps);
-        console.log("effected");
-    }, [updateSteps]);
+		// console.log(Pedometer.watchStepCount(updateSteps));
+        console.log("useEffect caught");
+    });
 
     const getSteps = () => {
         console.log(ped);
@@ -29,6 +47,9 @@ const HomeScreen = (props) => {
             <Text>Steps: {ped}</Text>
             <TouchableOpacity onPress={getSteps}>
                 <Text>Update steps</Text>
+            </TouchableOpacity>
+			<TouchableOpacity onPress={request}>
+                <Text>Request Permissions</Text>
             </TouchableOpacity>
         </View>
     );
