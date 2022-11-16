43f118ae9f15c2d6847759ef8e45072eeaf84137
TRAVIS HEAVENER (1035058) <1035058@lcps.org>
2022-11-16 12:14:24 -0500

    Updated to RN Stack Navigation

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
+
+
+const HomeScreen = (props) => {
+    return (
+        <View>
+            <Text>Steps: {0}</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default HomeScreen;
