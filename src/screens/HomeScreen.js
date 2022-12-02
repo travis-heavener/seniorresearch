442ee3a05357ae590f48a9d310a65b013cf92f72
TRAVIS HEAVENER (1035058) <1035058@lcps.org>
2022-12-02 11:56:23 -0500

    Fixed pedometer w/ EAS build

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,17 +1,41 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
 
+import { Pedometer } from "expo-sensors";
+import { PermissionsAndroid } from 'react-native';
 
 const HomeScreen = (props) => {
-    return (
-        <View>
-            <Text>Steps: {0}</Text>
-        </View>
-    );
+	const [steps, setSteps] = useState(0);
+    
+    let requestPerms = async () => {
+        let hasPerms = await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.ACTIVITY_RECOGNITION);
+
+        if (!hasPerms) {
+            console.log("Lacking ACTIVITY_RECOGNITION permissions, requesting...");
+            hasPerms = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACTIVITY_RECOGNITION);
+        }
+
+        return hasPerms;
+    };
+
+	const start = async () => {
+        let hasPerms = await requestPerms();
+
+        console.log("Has ACTIVITY_RECOGNITION permissions? " + hasPerms);
+
+		Pedometer.watchStepCount(res => setSteps(res.steps));
+	};
+
+	return (
+		<View>
+			<Text>Steps: {steps}</Text>
+			<TouchableOpacity onPress={start}>
+				<Text>Start Recording</Text>
+			</TouchableOpacity>
+		</View>
+	);
 };
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
 
 export default HomeScreen;
