d423db6ff8c413ce93fa843458956867ed1240bc
Travis H <travis.heavener@gmail.com>
2023-02-20 21:08:30 -0500

    Converted compass to react-native Animated

--- src/components/CompassWidget.js
+++ src/components/CompassWidget.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useFocusEffect } from "@react-navigation/native";
-import { StyleSheet, ImageBackground, Image, Text } from "react-native";
+import { StyleSheet, ImageBackground, Image, Text, Animated, View, Easing } from "react-native";
 
 import { Settings } from "../config/Config";
 
@@ -16,20 +16,16 @@ const NEEDLE_SRC = require(MEDIA_ROOT + "needle.png");
 const CompassWidget = (props) => {
     const userContext = React.useContext(UserDataContext);
 
-    const [alpha, setAlpha] = useState(null);
-    const alphaRef = React.useRef();
-    alphaRef.current = alpha;
-
-    const [lastAlpha, setLastAlpha] = useState(null);
-    const lastAlphaRef = React.useRef();
-    lastAlphaRef.current = lastAlpha;
+	const [lastAlpha, setLastAlpha] = useState(0);
+	const lastAlphaRef = useRef();
+	lastAlphaRef.current = lastAlpha;
 
     const [timeSinceUpdate, setTimeSinceUpdate] = useState(Date.now());
-    const timeSinceUpdateRef = React.useRef();
+    const timeSinceUpdateRef = useRef();
     timeSinceUpdateRef.current = timeSinceUpdate;
 
     // loads every time screen is focused
-    useFocusEffect(
+	useFocusEffect(
         React.useCallback(() => {
             let list = DeviceMotion.addListener(({ rotation }) => {
                 // manage callback speed
@@ -37,47 +33,48 @@ const CompassWidget = (props) => {
                 if (Date.now() - timeSinceUpdateRef.current < rate) return;
                 setTimeSinceUpdate(Date.now());
 
-                const format = n => (n < 0 ? n + 2*Math.PI : n) % (2*Math.PI);
-                
-                if (rotation == undefined) {
-                    return;
-                } else if (alphaRef.current == null) {
-                    setAlpha( format(rotation.alpha) );
-                    return;
-                } else if (lastAlphaRef.current == null) {
-                    setLastAlpha( format(alphaRef.current) );
-                    setAlpha( format(rotation.alpha) );
-                    return;
-                }
+				// calculate & queue rotation
+                if (rotation == undefined) return; // prevent empty information
 
-                // queue rotation
-                const maxTime = rate * 0.75;
+				// I hate this so much
+				// I spent so much time with this (~8 hrs) and converting
+				// to degrees was what fixed everyting.
+				const deg = n => n * 180 / Math.PI;
+				
+				// thanks stackoverflow
+				// https://stackoverflow.com/questions/19618745/css3-rotate-transition-doesnt-take-shortest-way/53416030#53416030
+				let nR = deg(rotation.alpha + (rotation.alpha < 0 ? 2*Math.PI : 0)); // scale from [-pi, pi] to [0, 360]
+				let rot = lastAlphaRef.current;
 
-                let alpha = format(rotation.alpha);
-                let initial = lastAlphaRef.current;
-                let frames = 45;
-                
-                let delta = alpha - initial;
-                if (Math.abs(delta) > Math.PI)
-                delta -= 2 * Math.PI * Math.sign(delta);
-                
-                let step = delta / frames;
-                
-                if (Math.abs(delta) < 0.1) return; // ignore needless calculations
+				let aR = rot % 360; // the apparent rotation, or how it is visibly shown (not the actual value which stores more rotations than [0,360])
+				if (aR < 0) aR += 360; // convert negative values to positive
+				if (aR < 180 && (nR > (aR + 180))) rot -= 360; // rotation over a 0/360 hitch
+				if (aR >= 180 && (nR <= (aR - 180))) rot += 360; // rotation over a 0/360 hitch
 
-                let interval = maxTime / frames;
-                
-                for (let i = 0; i < frames; i++) {
-                    setTimeout(function() {
-                        setAlpha(format(initial + step * (i+1)));
-                        if (i+1 == frames) setLastAlpha(format(alphaRef.current));
-                    }, i * interval);
-                }
+				rot += nR - aR; // add the rotation to the previous value
+
+				Animated.timing(rotateAnim, {
+					toValue: rot,
+					timing: rate * 0.75,
+					easing: Easing.linear,
+					useNativeDriver: true
+				}).start();
+
+				// store last rotation value
+				setLastAlpha(rot);
             });
 
             return () => list.remove();
         }, [props.navigation.isFocused()])
     );
+
+	const rotateAnim = useRef(new Animated.Value(0)).current;
+
+	// basically this formats the input angle to a range of strings the rotation can use
+	const alpha = rotateAnim.interpolate({
+		inputRange: [0, 360],
+		outputRange: ["0deg", "360deg"]
+	});
 
     return (
         // rotating needle
@@ -86,19 +83,28 @@ const CompassWidget = (props) => {
         // </ImageBackground>
 
         // rotating compass w/ counter-rotating needle
-        <ImageBackground style={[styles.wrapperImg, {transform: [{rotate: (alphaRef.current ?? 0) + "rad"}]}]} source={WRAPPER_SRC}>
-            <Image style={[styles.needleImg, {transform: [{rotate: (-alphaRef.current ?? 0) + "rad"}]}]} source={NEEDLE_SRC} />
-        </ImageBackground>
+        // <ImageBackground style={[styles.wrapperImg, {transform: [{rotate: (alphaRef.current ?? 0) + "rad"}]}]} source={WRAPPER_SRC}>
+        //     <Image style={[styles.needleImg, {transform: [{rotate: (-alphaRef.current ?? 0) + "rad"}]}]} source={NEEDLE_SRC} />
+        // </ImageBackground>
+		<View style={styles.top}>
+			<Animated.Image style={[styles.wrapperImg, {transform: [{rotate: alpha}]}]} source={WRAPPER_SRC} />
+			<Image style={styles.needleImg} source={NEEDLE_SRC} />
+		</View>
     );
 };
 
 const styles = StyleSheet.create({
-    wrapperImg: {
-        flex: 1,
+    top: {
+		flex: 1,
         justifyContent: "center",
         alignItems: "center"
+	},
+	wrapperImg: {
+        width: "100%",
+        height: "100%"
     },
     needleImg: {
+		position: "absolute",
         width: "75%",
         height: "75%"
     }
