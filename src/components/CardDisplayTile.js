581a7dd02c701855dc8d1ea84b1f498d422d74cb
Travis Heavener <1035058@lcps.org>
2023-05-17 13:15:36 -0400

    Added non-animated obj completion visual

--- src/components/CardDisplayTile.js
+++ src/components/CardDisplayTile.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { Pressable, StyleSheet } from "react-native";
+import { Pressable, StyleSheet, View } from "react-native";
 import { Themes } from "../config/Themes";
 import { vh } from "../config/Toolbox";
 import { UserDataContext } from "../config/UserDataManager";
@@ -9,6 +9,16 @@ const CardDisplayTile = (props) => {
     const userContext = useContext( UserDataContext );
     const THEME = Themes[ userContext.selectedTheme ].cards;
     const { onPress, row, col, obj } = props;
+
+    // objective completion progression for background color
+    const blobStyle = (obj.isCompleted || (obj.objType == "ExploreObjective" && obj.targetCount <= 1)) ? {display: "none"} : {
+        position: "absolute",
+        left: 0,
+        width: (obj.targetsFound / obj.targetCount * 100) + "%",
+        top: 0,
+        bottom: 0,
+        backgroundColor: THEME.checkedTile
+    };
 
     return (
         <Pressable
@@ -20,6 +30,7 @@ const CardDisplayTile = (props) => {
                 (row == 0) ? {borderTopWidth: vh(0.26)} : {}, (col == 0) ? {borderLeftWidth: vh(0.26)} : {} // borders for top/left
             ]}
         >
+            { obj.objType == "ExploreObjective" ? <View style={blobStyle} /> : null }
             {/* <Text numberOfLines={1} adjustsFontSizeToFit={true} style={styles.tileText}>{obj.toString()}</Text> */}
             <ScalingText maxLineLength={12}>{ obj.toString(userContext) }</ScalingText>
         </Pressable>
@@ -31,6 +42,7 @@ export default CardDisplayTile;
 const styles = StyleSheet.create({
     tile: {
         flex: 1/5,
+        flexDirection: "row",
         justifyContent: "center",
         borderColor: "black",
         borderRightWidth: vh(0.26),
